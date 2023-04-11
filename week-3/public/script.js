cebf33d9ade1443dd0f9eb00f185e4caefe87877
Cindy <wscindy.lee@gmail.com>
2023-04-11 23:24:43 +0800

    week-3 edited v1

--- week-3/public/script.js
+++ week-3/public/script.js
@@ -1,9 +1,9 @@
 
 const { response } = require("express")
 
-function loadDoc01(){
+function loadDoc01() {
 
-document.getElementById("p").innerHTML = 'text'
+    document.getElementById("p").innerHTML = 'text'
 }
 
 function loadDoc() {
@@ -11,35 +11,24 @@ function loadDoc() {
 
     var xhr = new XMLHttpRequest()
 
-    // xhr.open('get','http://localhost:3000/getData')
 
     num = document.getElementById('input').value
 
-    // num = document.getElementById('input').value
-    // fetch('http://localhost:3000/getData?number='+num)
-    // .then(response){
-    // console.log(response);
-    // document.getElementById("p").innerHTML = response;}
-    // .catch((error) => console.log(error))
+    fetch('http://localhost:3000/getData?number=' + num, {})
+        .then((response) => {
+            // 這裡會得到一個 ReadableStream 的物件
+            console.log(response);
+            response.text().then(function (text) {
 
+                document.getElementById("p").innerHTML = text;
+            });
+            // 可以透過 blob(), json(), text() 轉成可用的資訊
 
-    
-    fetch('http://localhost:3000/getData?number='+num, {})
-    .then((response) => {
-      // 這裡會得到一個 ReadableStream 的物件
-      console.log(response);
-      response.text().then(function (text) {
-        
-        document.getElementById("p").innerHTML = text;
-      });
-      // 可以透過 blob(), json(), text() 轉成可用的資訊
-      
-    }).catch((err) => {
-      console.log('錯誤:', err);
-      
-  });
-    // src = 'http://localhost:3000/getData?number=' + num.value
-    // document.getElementById("p").innerHTML = response.text()
+        }).catch((err) => {
+            console.log('錯誤:', err);
+
+        });
+
 
 }
 
