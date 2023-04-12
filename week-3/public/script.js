377a3e0b7f9a8f6b5043914f14714cf9fe7b4ca3
Cindy <wscindy.lee@gmail.com>
2023-04-12 15:46:55 +0800

    week-3 edit public/script

--- week-3/public/script.js
+++ week-3/public/script.js
@@ -10,6 +10,8 @@ function loadDoc() {
 
     num = document.getElementById('input').value
 
+
+
     fetch('http://localhost:3000/getData?number=' + num, {})
         .then((response) => {
             // 這裡會得到一個 ReadableStream 的物件
@@ -23,6 +25,20 @@ function loadDoc() {
         }).catch((err) => {
             console.log('錯誤:', err);
 
+            fetch('https://remote-assignments.onrender.com/getData?number=' + num, {})
+            .then((response) => {
+                // 這裡會得到一個 ReadableStream 的物件
+                console.log(response);
+                response.text().then(function (text) {
+    
+                    document.getElementById("p").innerHTML = text;
+                });
+                // 可以透過 blob(), json(), text() 轉成可用的資訊
+    
+            }).catch((err) => {
+                console.log('錯誤:', err);
+    
+            });
         });
 
 
