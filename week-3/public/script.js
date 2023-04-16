4245f2427acadfa60625073fa7bbed8be3d2b775
Cindy <wscindy.lee@gmail.com>
2023-04-16 23:22:45 +0800

    week-3 edit fetch function

--- week-3/public/script.js
+++ week-3/public/script.js
@@ -1,18 +1,13 @@
 
 const { response } = require("express")
 
-function loadDoc01() {
-
-    document.getElementById("p").innerHTML = 'text'
-}
-
 function loadDoc() {
 
     num = document.getElementById('input').value
 
 
 
-    fetch('http://localhost:3000/getData?number=' + num, {})
+    fetch('http://localhost:3000/getData?number=' + num, {} || 'https://remote-assignments.onrender.com/getData?number=' + num, {})
         .then((response) => {
             // 這裡會得到一個 ReadableStream 的物件
             console.log(response);
@@ -25,20 +20,20 @@ function loadDoc() {
         }).catch((err) => {
             console.log('錯誤:', err);
 
-            fetch('https://remote-assignments.onrender.com/getData?number=' + num, {})
-                .then((response) => {
-                    // 這裡會得到一個 ReadableStream 的物件
-                    console.log(response);
-                    response.text().then(function (text) {
+            // fetch('https://remote-assignments.onrender.com/getData?number=' + num, {})
+            //     .then((response) => {
+            //         // 這裡會得到一個 ReadableStream 的物件
+            //         console.log(response);
+            //         response.text().then(function (text) {
 
-                        document.getElementById("p").innerHTML = text;
-                    });
-                    // 可以透過 blob(), json(), text() 轉成可用的資訊
+            //             document.getElementById("p").innerHTML = text;
+            //         });
+            //         // 可以透過 blob(), json(), text() 轉成可用的資訊
 
-                }).catch((err) => {
-                    console.log('錯誤:', err);
+            //     }).catch((err) => {
+            //         console.log('錯誤:', err);
 
-                });
+            //     });
         });
 
 
