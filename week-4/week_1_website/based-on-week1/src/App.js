ff3cf72d5dd7188efed27257b4db13b985fd0ea8
Cindy <wscindy.lee@gmail.com>
2023-04-25 00:42:23 +0800

    week-4 sidebar edited 2nd

--- week-4/week_1_website/based-on-week1/src/App.js
+++ week-4/week_1_website/based-on-week1/src/App.js
@@ -2,13 +2,19 @@ import './App.css';
 import NavBar from './NavBar';
 import WelcomeMsg from './WelcomMsg';
 import ContentBox from './ContentBox';
-import CallToAction from './CallToAction';
-
+import { useState } from 'react';
 
 
 
 
 function App() {
+
+  const [toggle, setToggle] = useState(false);
+
+  function handleToggle() {
+    setToggle(!toggle);
+  };
+
 
   return (
     <div className="App">
@@ -16,7 +22,11 @@ function App() {
       <WelcomeMsg section="Section Title" />
       <ContentBox boxName="Content Box 1" boxNameTwo="Content Box 2" />
       <ContentBox boxName="Content Box 3" boxNameTwo="Content Box 4" />
-      <CallToAction btnName="Call to Action" />
+      <div className="btn">
+        <button className="btn_action btn" onClick={handleToggle}>Call to Action</button>
+      </div>
+      {toggle ? <ContentBox boxName="Content Box 5" boxNameTwo="Content Box 6" /> : null}
+      {toggle ? <ContentBox boxName="Content Box 7" boxNameTwo="Content Box 8" /> : null}
     </div>
   );
 }
--- week-4/week_1_website/based-on-week1/src/ContentBox.js
+++ week-4/week_1_website/based-on-week1/src/ContentBox.js
@@ -2,9 +2,9 @@ import React from "react";
 
 function ContentBox(props) {
     return (
-        <div class="box" id="box">
-            <div class="items">{props.boxName}</div>
-            <div class="items">{props.boxNameTwo}</div>
+        <div className="box" id="box">
+            <div className="items">{props.boxName}</div>
+            <div className="items">{props.boxNameTwo}</div>
         </div>
     );
 }
